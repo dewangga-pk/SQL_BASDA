4d4d7c3def689958011d4ea8d25f622f14a0abc8
dewangga-pk <pacetcommunity570@gmail.com>
2019-10-24 15:27:33 +0700

    Update Query Modul 3.sql

--- Praktikum/Query Modul 3.sql
+++ Praktikum/Query Modul 3.sql
@@ -11,29 +11,14 @@ begin
 	select @bil2 = 15
 	select @selisih = @bil1-@bil2
 	print @selisih
-	
 	if @bil1>=@bil2
 		begin
 		print('Bilangan 1 lebih atau sama dengan bilangan 2')
-		if (@bil1-@bil2)<0
-			begin
-			print('Selisih antara bilangan 1 dan bilangan 2 bernilai positif : '+str(@bil2-@bil1))
-			end
-		else
-			begin
-			print('Selisih antara bilangan 1 dan bilangan 2 bernilai positif : '+str(@selisih))
-			end
+		print('Selisih antara bilangan 1 dan bilangan 2 bernilai positif : '+str(@selisih))
 		end
-	else 
+	else
 		begin
 		print('Bilangan 1 kurang atau sama dari bilangan 2')
-		if (@bil1-@bil2)<0
-			begin
-			print('Selisih antara bilangan 1 dan bilangan 2 bernilai positif : '+str(@bil2-@bil1))
-			end
-		else
-			begin
-			print('Selisih antara bilangan 1 dan bilangan 2 bernilai positif : '+str(@selisih))
-			end
+		print('Selisih antara bilangan 1 dan bilangan 2 bernilai negatif : '+str(@selisih))
 		end
 end
